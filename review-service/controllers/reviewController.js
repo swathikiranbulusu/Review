78d0a056076318bab4c6944c1f0f54f4746c0338
swathikiranbulusu <146068786+swathikiranbulusu@users.noreply.github.com>
2025-05-09 13:52:09 +0530

    Update reviewController.js

--- review-service/controllers/reviewController.js
+++ review-service/controllers/reviewController.js
@@ -1,12 +1,22 @@
 const Review = require('../models/Review');
 
-exports.addReview = async (req, res) => {
-  const review = new Review(req.body);
-  await review.save();
-  res.json(review);
+exports.createReview = async (req, res) => {
+  try {
+    const review = new Review(req.body);
+    await review.save();
+    res.status(201).json(review);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.getAllReviews = async (req, res) => {
+  const reviews = await Review.find();
+  res.json(reviews);
 };
 
 exports.getReviewsByMovie = async (req, res) => {
-  const reviews = await Review.find({ movieId: req.params.movieId });
+  const { movieId } = req.params;
+  const reviews = await Review.find({ movieId });
   res.json(reviews);
 };
